6d489d59ffd77c22348ff7bbc40dd4a5d4b459b8
muskansandhay <113002020+muskansandhay@users.noreply.github.com>
2023-06-25 21:16:04 -0400

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Assignment1
+{
+    //properties
+    public class Patient
+    {
+        public string _firstName;
+        public string _lastName;
+        public double _weight;
+        public double _height;
+
+        //constructors
+        public Patient(string fname, string lname, double height, double weigth)
+        {
+            _firstName = fname;
+            _lastName = lname;
+            _weight = weigth;
+            _height = height;
+
+        }
+
+        public Patient() { }
+
+        //methods
+        public string calculateBP(int SYSTOLIC,int DIASTOLIC)
+        {
+            string msg;
+            if (SYSTOLIC<120&& DIASTOLIC<80) {
+                msg = "normal";
+            }else if ((SYSTOLIC >= 120&& SYSTOLIC<=129 )&& (DIASTOLIC <80))
+            {
+                msg = "Elevated";
+            }else if ((SYSTOLIC >= 130 && SYSTOLIC<=139 )|| (DIASTOLIC >=80 && DIASTOLIC <= 89))
+            {
+                msg = "High Blood Pressure(Hypertension stage 1)";
+           }else if (SYSTOLIC >=140 && DIASTOLIC >=90 )
+            {
+                msg = "High Blood Pressure(Hypertension stage 2)";
+            }
+            else if (SYSTOLIC < 180 && DIASTOLIC < 120)
+            {
+                msg = "HYPERTENSIVE CRISIS( Consult a doctor )";
+            }
+            else
+            {
+                msg = "not a valid input";
+            }
+            return msg;
+        }
+
+        public double calculateBMI()
+        {
+            return (_weight *10000/ (_height * _height));
+        }
+
+        public void printingInfo()
+        {
+            Console.WriteLine("fullName: " + _firstName + " " + _lastName);
+            Console.WriteLine("weight: " + _weight);
+            Console.WriteLine("Height: " + _height);
+            Console.WriteLine("blood pressure: "+calculateBP(120,80));
+            Console.WriteLine("BMI: " + calculateBMI());
+
+            double bmi = calculateBMI();
+            if (bmi >= 25.0)
+            {
+                Console.WriteLine("BMI Status: Overweight");
+            }
+            else if (bmi >= 18.5 && bmi <= 24.9)
+            {
+                Console.WriteLine("BMI Status: Normal (In the healthy range)");
+            }
+            else
+            {
+                Console.WriteLine("BMI Status: Underweight");
+            }
+
+
+        }
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Assignment1
+{
+
+    public class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the system");
+
+            //asking the user to input patient
+            Console.Write("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //asking user to insert patient information
+            Console.WriteLine("\n Enter the patient information:");
+            Console.Write("First Name:");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Weight (in KG): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Height (in CM): ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            //Create a Patient object based on user input
+            Patient patient = new Patient(firstName, lastName, height, weight);
+
+            //printing patient information
+            patient.printingInfo();
+            Console.ReadLine();
+        }
+    }
+}
